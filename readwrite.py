8a6d1d696cc99a203ed26dfbccfaeb205fc74f01
ssakthi <ssakthi@iu.edu>
2022-10-06 01:19:02 -0400

    push

---
+++ readwrite.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+from gspread_pandas import Spread, Client
+import numpy as np
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = Credentials.from_service_account_file('./ecmganalystdashboard-151feef86862.json', scopes=scope)
+
+
+client = Client(scope=scope, creds=credentials)
+spread = Spread("ReadWrite", client=client)
+
+stock_df = pd.DataFrame(np.array([[1, 2, 2], [4, 5, 6], [7, 8, 9]]), columns=['a', 'z', 'on'])
+
+spread.df_to_sheet(stock_df)
+
+stock_df = spread.sheet_to_df()
+
+st.write(stock_df)
